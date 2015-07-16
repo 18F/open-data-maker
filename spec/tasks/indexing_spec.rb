a926669e4325a53196c8692d8f714f932df50e99
Sarah Allen <sarah.allen@gsa.gov>
2015-07-15 19:48:50 -0700

    We want to the rake task (specs pass, except for 1 pending)

--- spec/tasks/indexing_spec.rb
+++ spec/tasks/indexing_spec.rb
@@ -1,22 +1,26 @@
 require 'spec_helper'
+require 'bundler/setup'
+require 'padrino-core/cli/rake'
 
-describe 'elastic search index management tasks' do
-
+describe 'elastic search index management rake tasks' do
+  before do
+    PadrinoTasks.init
+    DataMagic::Config.init
+  end
+  
   context "imports" do
-    before do
-      DataMagic.delete_index('cities')
-      DataMagic.delete_index('places')
-    end
-
     it "default sample-data" do
-      expect { DataMagic.import_all }.not_to raise_exception
+      ENV['DATA_PATH'] = nil
+      expect { Rake::Task['import'].invoke }.not_to raise_exception
+      DataMagic.delete_index('city-data')
     end
 
     it "correct configuration" do
       dir_path = './spec/fixtures/import_all'
       ENV['DATA_PATH'] = dir_path
-      expect { DataMagic.import_all }.not_to raise_exception
+      expect { Rake::Task['import'].invoke }.not_to raise_exception
       expect(DataMagic::Config.api_endpoint_names).to eq(['cities'])
+      DataMagic.delete_index('city-data')
     end
 
   end
