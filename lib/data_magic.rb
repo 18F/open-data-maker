9c35902b2361ad8fa0950de8d3e0c9a871ea4052
Sarah Allen <sarah.allen@gsa.gov>
2015-07-26 06:54:34 -0700

    library supports returning a subset of attributes

--- lib/data_magic.rb
+++ lib/data_magic.rb
@@ -58,9 +58,11 @@ module DataMagic
   # thin layer on elasticsearch query
   def self.search(terms, options = {})
     terms = IndifferentHash.new(terms)
+    options[:fields] ||= []
+    fields = options[:fields].map { |field| field.to_s }
     index_name = index_name_from_options(options)
     logger.info "search terms:#{terms.inspect}"
-    squery = Stretchy.query(type: 'document')
+    squery = Stretchy.query
 
     distance = terms[:distance]
     if distance && !distance.empty?
@@ -89,6 +91,9 @@ module DataMagic
         query: squery.to_search
       }
     }
+    if not fields.empty?
+      full_query[:body][:fields] = fields
+    end
 
     logger.info "===========> full_query:#{full_query.inspect}"
 
@@ -96,8 +101,25 @@ module DataMagic
     logger.info "result: #{result.inspect}"
     hits = result["hits"]
     total = hits["total"]
-    hits["hits"].map {|hit| hit["_source"]}
-    results = hits["hits"].map {|hit| hit["_source"]}
+    results = []
+    if fields.empty?
+      # we're getting the whole document and we can find in _source
+      results = hits["hits"].map {|hit| hit["_source"]}
+    else
+      # we're getting a subset of fields...
+      results = hits["hits"].map do |hit|
+        found = hit["fields"]
+        # each result looks like this:
+        # {"city"=>["Springfield"], "address"=>["742 Evergreen Terrace"]}
+
+        found.keys.each { |key| found[key] = found[key][0] }
+        # now it should look like this:
+        # {"city"=>"Springfield", "address"=>"742 Evergreen Terrace
+        found
+      end
+    end
+
+    # assemble a simpler json document to return
     {
       "total" => total,
       "page" => page,
