72a08cefc059342accebdcacfdad806479fd3d73
Sarah Allen <sarah.allen@gsa.gov>
2015-08-13 05:40:24 -0600

    additional tests for nested fields these mysteriously pass without code changes

--- spec/features/api_spec.rb
+++ spec/features/api_spec.rb
@@ -18,161 +18,191 @@ end
 
 
 describe 'api', type: 'feature' do
-	# app starts up in advance of before :all so for now testing only
-	# with ./sample-data
+	context 'with sample data' do
+		# app starts up in advance of before :all so for now testing only
+		# with ./sample-data
 
-	after(:all) do
-		Stretchy.delete 'test-city-data'
-		#expect(DataMagic.client.indices.get(index: '_all')).to be_empty
-	end
-
-  it "loads the endpoint list" do
-    get "/endpoints"
-
-    expect(last_response).to be_ok
-    expect(last_response.content_type).to eq('application/json')
-
-    result = JSON.parse(last_response.body)
-    expected = {
-      'endpoints'=>[
-        'name'=>'cities',
-        'url'=>'/cities',
-      ]
-    }
-    expect(result).to eq expected
-  end
-
-  it "raises a 404 on missing endpoints" do
-    get "/missing"
-    expect(last_response.status).to eq(404)
-    expect(last_response.content_type).to eq('application/json')
-
-    result = JSON.parse(last_response.body)
-    expected = {
-      "error"=>404,
-      "message"=>"missing not found. Available endpoints: cities",
-    }
-    expect(result).to eq(expected)
-  end
-
-	describe "data description" do
-		before do
-			get '/data.json'
+		after(:all) do
+			Stretchy.delete 'test-city-data'
+			#expect(DataMagic.client.indices.get(index: '_all')).to be_empty
 		end
 
-		it_behaves_like "api request"
+	  it "loads the endpoint list" do
+	    get "/endpoints"
 
-		it "responds with json" do
-			expect(last_response).to be_ok
-			expect(last_response.content_type).to eq('application/json')
-		end
-	end
+	    expect(last_response).to be_ok
+	    expect(last_response.content_type).to eq('application/json')
 
-	describe "query" do
-		describe "with one term" do
+	    result = JSON.parse(last_response.body)
+	    expected = {
+	      'endpoints'=>[
+	        'name'=>'cities',
+	        'url'=>'/cities',
+	      ]
+	    }
+	    expect(result).to eq expected
+	  end
+
+	  it "raises a 404 on missing endpoints" do
+	    get "/missing"
+	    expect(last_response.status).to eq(404)
+	    expect(last_response.content_type).to eq('application/json')
+
+	    result = JSON.parse(last_response.body)
+	    expected = {
+	      "error"=>404,
+	      "message"=>"missing not found. Available endpoints: cities",
+	    }
+	    expect(result).to eq(expected)
+	  end
+
+		describe "data description" do
 			before do
-				get '/cities?name=Chicago'
+				get '/data.json'
 			end
 
 			it_behaves_like "api request"
 
 			it "responds with json" do
-			  expect(last_response).to be_ok
-				expect(last_response.content_type).to eq('application/json')
-
-				result = JSON.parse(last_response.body)
-
-				expected = {
-					"total" => 1,
-				  "page"  => 0,
-				  "per_page" => DataMagic::DEFAULT_PAGE_SIZE,
-				  "results" => [
-						{"state"=>"IL", "name"=>"Chicago", "population"=>2695598,
-						 "land_area"=>227.635,   # later we'll make this a float
-						 "location"=>{"lat"=>41.837551, "lon"=>-87.681844}}						]
-				}
-				expect(result).to eq(expected)
-
-			end
-		end
-		describe "with options" do
-			it "can return a subset of fields" do
-				get '/cities?state=MA&fields=name,population'
-
 				expect(last_response).to be_ok
-				result = JSON.parse(last_response.body)
-
-				expected = {
-					"total" => 1,
-					"page"  => 0,
-					"per_page" => DataMagic::DEFAULT_PAGE_SIZE,
-					"results" => [{"name"=>"Boston", "population"=>617594}]
-				}
-				expect(result).to eq(expected)
-
+				expect(last_response.content_type).to eq('application/json')
 			end
-
 		end
 
-		describe "with float" do
-			before do
-				get '/cities?land_area=302.643'
-			end
+		describe "query" do
+			describe "with one term" do
+				before do
+					get '/cities?name=Chicago'
+				end
 
-			it "responds with json" do
-			  expect(last_response).to be_ok
-				expect(last_response.content_type).to eq('application/json')
+				it_behaves_like "api request"
 
-				result = JSON.parse(last_response.body)
+				it "responds with json" do
+				  expect(last_response).to be_ok
+					expect(last_response.content_type).to eq('application/json')
 
-				expected = {
-					"total" => 1,
-				  "page"  => 0,
-				  "per_page" => DataMagic::DEFAULT_PAGE_SIZE,
-				  "results" => [{"state"=>"NY", "name"=>"New York",
-						"population"=>8175133, "land_area"=>302.643,
-						"location"=>{"lat"=>40.664274, "lon"=>-73.9385}}]
+					result = JSON.parse(last_response.body)
+
+					expected = {
+						"total" => 1,
+					  "page"  => 0,
+					  "per_page" => DataMagic::DEFAULT_PAGE_SIZE,
+					  "results" => [
+							{"state"=>"IL", "name"=>"Chicago", "population"=>2695598,
+							 "land_area"=>227.635,   # later we'll make this a float
+							 "location"=>{"lat"=>41.837551, "lon"=>-87.681844}}						]
 					}
-				expect(result).to eq(expected)
+					expect(result).to eq(expected)
+
+				end
+			end
+			describe "with options" do
+				it "can return a subset of fields" do
+					get '/cities?state=MA&fields=name,population'
+
+					expect(last_response).to be_ok
+					result = JSON.parse(last_response.body)
+
+					expected = {
+						"total" => 1,
+						"page"  => 0,
+						"per_page" => DataMagic::DEFAULT_PAGE_SIZE,
+						"results" => [{"name"=>"Boston", "population"=>617594}]
+					}
+					expect(result).to eq(expected)
+
+				end
 
 			end
 
-		end
-		describe "near zipcode" do
-			before do
-				get '/cities?zip=94132&distance=30mi'
-				# why isn't SF, 30 miles from SFO... maybe origin point is not
-				# where I expect
-			end
+			describe "with float" do
+				before do
+					get '/cities?land_area=302.643'
+				end
 
-			it "can find an attribute from an imported file" do
-				expect(last_response).to be_ok
-				result = JSON.parse(last_response.body)
-				result["results"] = result["results"].sort_by { |k| k["name"] }
+				it "responds with json" do
+				  expect(last_response).to be_ok
+					expect(last_response.content_type).to eq('application/json')
 
-				expected = {
-				  "total" => 2,
-				  "page"  => 0,
-				  "per_page" => DataMagic::DEFAULT_PAGE_SIZE,
-				  "results" => [
-						{"state"=>"CA", "name"=>"Fremont", "population"=>214089, "land_area"=>77.459, "location"=>{"lat"=>37.494373, "lon"=>-121.941117}},
-						{"state"=>"CA", "name"=>"Oakland", "population"=>390724, "land_area"=>55.786, "location"=>{"lat"=>37.769857, "lon"=>-122.22564}}]
-				}
-				expect(result).to eq(expected)
-			end
-		end
+					result = JSON.parse(last_response.body)
 
-		describe "with sort" do
-			it "can sort numbers ascending" do
-				get '/cities?sort=population:asc'
-				expect(last_response).to be_ok
-				response = JSON.parse(last_response.body)
-				expect(response["results"][0]['name']).to eq("Rochester")
+					expected = {
+						"total" => 1,
+					  "page"  => 0,
+					  "per_page" => DataMagic::DEFAULT_PAGE_SIZE,
+					  "results" => [{"state"=>"NY", "name"=>"New York",
+							"population"=>8175133, "land_area"=>302.643,
+							"location"=>{"lat"=>40.664274, "lon"=>-73.9385}}]
+						}
+					expect(result).to eq(expected)
+
+				end
 
 			end
+			describe "near zipcode" do
+				before do
+					get '/cities?zip=94132&distance=30mi'
+					# why isn't SF, 30 miles from SFO... maybe origin point is not
+					# where I expect
+				end
+
+				it "can find an attribute from an imported file" do
+					expect(last_response).to be_ok
+					result = JSON.parse(last_response.body)
+					result["results"] = result["results"].sort_by { |k| k["name"] }
+
+					expected = {
+					  "total" => 2,
+					  "page"  => 0,
+					  "per_page" => DataMagic::DEFAULT_PAGE_SIZE,
+					  "results" => [
+							{"state"=>"CA", "name"=>"Fremont", "population"=>214089, "land_area"=>77.459, "location"=>{"lat"=>37.494373, "lon"=>-121.941117}},
+							{"state"=>"CA", "name"=>"Oakland", "population"=>390724, "land_area"=>55.786, "location"=>{"lat"=>37.769857, "lon"=>-122.22564}}]
+					}
+					expect(result).to eq(expected)
+				end
+			end
+
+			describe "with sort" do
+				it "can sort numbers ascending" do
+					get '/cities?sort=population:asc'
+					expect(last_response).to be_ok
+					response = JSON.parse(last_response.body)
+					expect(response["results"][0]['name']).to eq("Rochester")
+
+				end
 
 
+			end
 		end
 	end
 
+	context "with nested data" do
+		before do
+			DataMagic.destroy
+			ENV['DATA_PATH'] = './spec/fixtures/nested_files'
+			DataMagic.config = DataMagic::Config.new
+	    DataMagic.import_with_dictionary
+		end
+		after do
+			DataMagic.destroy
+		end
+
+		it "can search" do
+			get '/school?name=Inquisitive Farm'
+			expect(last_response).to be_ok
+			result = JSON.parse(last_response.body)
+			expect(result["total"]).to eq(1)
+			first = result["results"].first
+			expect(first["name"]).to eq("Inquisitive Farm College")
+		end
+		it "can search for nested number" do
+			get '/school?2013.earnings.median=26318'
+			expect(last_response).to be_ok
+			result = JSON.parse(last_response.body)
+			expect(result["total"]).to eq(1)
+			first = result["results"].first
+			expect(first['2013']['earnings']).to eq({"percent_gt_25k"=>0.53, "median"=>26318})
+		end
+	end
 end
