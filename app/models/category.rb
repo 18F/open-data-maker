e9791874bf420fbd54e0a0c1e0475ba6bafef726
Shauna Haines <slhaines@gmail.com>
2015-08-18 22:20:55 -0700

    Fix whitespace issue

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,9 @@
 Category = Struct.new(:name, :description, :fields) do
   class << self
-  	def from_yml
+    def from_yml
       DataMagic.config.data['categories'].map do |key, value|
         new(value['title'], value['description'], ['field1', 'field2'])
       end
-  	end
+    end
   end
 end
