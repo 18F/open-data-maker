fcc853d126c3c14cf725cae34610a56dfd7f0f98
Ryan Buckley <arebuckley@gmail.com>
2015-08-19 21:51:04 -0700

    [#95 #107] Searching by name requires an exact match

--- spec/lib/data_magic_spec.rb
+++ spec/lib/data_magic_spec.rb
@@ -10,4 +10,18 @@ describe DataMagic do
     #expect(DataMagic.client.indices.get(index: '_all')).to be_empty
   end
 
+  describe '.prepare_field_types' do
+    it 'returns the given fields with their specified type' do
+      expect(described_class.prepare_field_types({ 'state' => 'string', land_area: 'string' }))
+      .to eq("state" => { :type => "string" }, :land_area => { :type => "string" })
+    end
+
+    context 'when the key is "name"' do
+      it 'returns type with :index of "not_analyzed"' do
+        expect(described_class.prepare_field_types({ 'state' => 'string', 'name' => 'string' }))
+        .to eq({"state"=>{:type=>"string"}, "name"=>{:type=>"string", :index=>"not_analyzed"}})
+      end
+    end
+  end
+
 end
