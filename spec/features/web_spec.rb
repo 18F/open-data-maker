73f8e187598ea9ba878b371d5eef62e43e7be277
Shauna Haines <slhaines@gmail.com>
2015-09-08 00:04:49 -0700

    Add spec tests for category page

--- spec/features/web_spec.rb
+++ spec/features/web_spec.rb
@@ -24,7 +24,15 @@ describe 'app', type: 'feature' do
   it "should display a list of categories" do
     get '/'
     expect(last_response.body).to include('Browse Data Details by Category')
-    expect(last_response.body).to include('General') #category name
+    expect(last_response.body).to include('General') # category name
     expect(last_response.body).to include('general information about the city, including standard identifiers')
+  end
+
+  it "should load the correct category page" do
+    get '/category/general'
+    expect(last_response.body).to include('Data Details for the')
+    expect(last_response.body).to include('category_details = {"title":"General"')
+    expect(last_response.body).to include('population') # a field name
+    expect(last_response.body).to include('The name of the city') # a field description
   end
 end
