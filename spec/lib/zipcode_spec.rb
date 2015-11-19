88bf74a2f948bcf559fee0dad9b25f5f274ec4e9
Matt Mills <sunrisetimes@gmail.com>
2015-11-18 20:35:49 -0800

    Validate zipcode params
    Per the issue (https://github.com/18F/open-data-maker/issues/213)
    invalid zipcodes were causing issues. This adds a validation to the
    Zipcode class, and a method to ErrorChecker to ensure that, if present,
    a supplied zipcode is valid.

--- spec/lib/zipcode_spec.rb
+++ spec/lib/zipcode_spec.rb
@@ -9,4 +9,13 @@ describe Zipcode do
     location = Zipcode.latlon(94132)
     expect(location).to eq(lat: 37.7211, lon: -122.4754)
   end
+
+  describe '#valid' do
+    it "returns true if the zipcode is valid" do
+      expect(Zipcode.valid? 94132).to eq(true)
+    end
+    it "returns false if the zipcode is invalid" do
+      expect(Zipcode.valid? 00002).to eq(false)
+    end
+  end
 end
