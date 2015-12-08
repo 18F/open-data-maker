60df89e425bac64fd7cc134628ff9b8efaaba937
Sarah Allen <sarah.allen@gsa.gov>
2015-12-08 11:42:52 -0800

    specs for next set of expressions needed
    https://github.com/18F/open-data-maker/issues/243

--- spec/lib/expression_spec.rb
+++ spec/lib/expression_spec.rb
@@ -2,9 +2,33 @@ require 'expression'
 
 describe Expression do
   context "simple or expression" do
-    it "can find variables" do
+    it "can find two variables" do
       expr = "ONE or TWO"
       expect(Expression.new(expr).variables).to eq(%w(ONE TWO))
+    end
+    it "can find multiple variables" do
+      expr = "ONE or TWO or THREE"
+      expect(Expression.new(expr).variables).to eq(%w(ONE TWO))
+    end
+    it "evaluates: false OR true to be true" do
+      expr = "f or t"
+      values = {f:0, t:1}
+      expect(Expression.new(expr).evaluate(values)).to eq(true)
+    end
+    it "evaluates: true OR false to be true" do
+      expr = "t or f"
+      values = {f:0, t:1}
+      expect(Expression.new(expr).evaluate(values)).to eq(true)
+    end
+    it "evaluates: false OR false to be false" do
+      expr = "f1 or f2"
+      values = {f1:0, f2:0}
+      expect(Expression.new(expr).evaluate(values)).to eq(false)
+    end
+    it "evaluates: true OR true to be true" do
+      expr = "t1 or t2"
+      values = {t1:0, t2:0}
+      expect(Expression.new(expr).evaluate(values)).to eq(true)
     end
   end
 end
