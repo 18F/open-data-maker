a600bfc155054a039e62e0c207b3d795142db73c
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-02-17 11:30:25 -0800

    Extracts a client/repository combination object
            This feels more like an enhanced client combined with a
    repository type object. The thing that is bothering me is all the
    document passing, which implies something with missing state.

--- lib/data_magic/index.rb
+++ lib/data_magic/index.rb
@@ -6,15 +6,6 @@ require 'action_view'  # for distance_of_time_in_words (logging time)
 include ActionView::Helpers::DateHelper  # for distance_of_time_in_words (logging time)
 
 module DataMagic
-  def self.get_id(doc)
-    return nil if config.data['unique'].length == 0
-    config.data['unique'].map { |field| doc[field] }.join(':')
-  end
-
-  def self.remove_ids(doc)
-    config.data['unique'].each { |key| doc.delete key }
-  end
-
   class Document
     attr_reader :data, :id
 
@@ -51,6 +42,79 @@ module DataMagic
     end
   end
 
+  class IndexClient
+    attr_reader :client, :options
+
+    def initialize(client, options)
+      @client = client
+      @options = options
+    end
+
+    def save(document)
+      @skipped_last_document = false
+      if creating?
+        create(document)
+      else
+        update(document)
+      end
+    end
+
+    def skipped_last_document?
+      @skipped_last_document
+    end
+
+    private
+
+    def create(document)
+      client.index({
+        index: index_name,
+        id: document.id,
+        type: 'document',
+        body: document.data,
+      })
+    end
+
+    def update(document)
+      document.remove_ids
+      if allows_skips?
+        update_with_rescue(document)
+      else
+        update_without_rescue(document)
+      end
+    end
+
+    def update_without_rescue(document)
+      client.update({
+        index: index_name,
+        id: document.id,
+        type: 'document',
+        body: {doc: document.data},
+      })
+    end
+
+    def update_with_rescue(document)
+      update_with_rescue(document)
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound
+      @skipped_last_document = true
+    end
+
+    def creating?
+      options[:nest] == nil
+    end
+
+    def allows_skips?
+      options[:nest][:parent_missing] == 'skip'
+    end
+
+    def index_name
+      config.scoped_index_name
+    end
+
+    def config
+      DataMagic.config
+    end
+  end
+
   # data could be a String or an io stream
   def self.import_csv(data, options={})
     self.create_index unless config.index_exists?
@@ -61,6 +125,8 @@ module DataMagic
     builder_data.log_metadata
 
     output = Output.new
+
+    index_client = IndexClient.new(client, options)
 
     begin
       CSV.parse(
@@ -81,30 +147,33 @@ module DataMagic
                       "in row: #{document.preview(255)}"
         end
 
-        if options[:nest] == nil  #first time or normal case
-          client.index({
-            index: es_index_name,
-            id: document.id,
-            type: 'document',
-            body: document.data,
-          })
-        else
-          begin
-            document.remove_ids
-            client.update({
-              index: es_index_name,
-              id: document.id,
-              type: 'document',
-              body: {doc: document.data},
-            })
-          rescue Elasticsearch::Transport::Transport::Errors::NotFound => e
-            if options[:nest][:parent_missing] == 'skip'
-              output.skipping(id)
-            else
-              raise e
-            end
-          end
-        end
+        index_client.save(document)
+        output.skipping(document.id) if index_client.skipped_last_document?
+
+        #if options[:nest] == nil  #first time or normal case
+          #client.index({
+            #index: es_index_name,
+            #id: document.id,
+            #type: 'document',
+            #body: document.data,
+          #})
+        #else
+          #begin
+            #document.remove_ids
+            #client.update({
+              #index: es_index_name,
+              #id: document.id,
+              #type: 'document',
+              #body: {doc: document.data},
+            #})
+          #rescue Elasticsearch::Transport::Transport::Errors::NotFound => e
+            #if options[:nest][:parent_missing] == 'skip'
+              #output.skipping(id)
+            #else
+              #raise e
+            #end
+          #end
+        #end
 
         output.increment
 
