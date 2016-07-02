e2a2fec61f934a70812d5b341e2508ea4b122ae7
kynetiv <miles@kynetiv.com>
2016-07-01 23:14:28 -0400

    fix withdotkeys loosing ancestor keys on deep hashes

--- spec/lib/nested_hash_spec.rb
+++ spec/lib/nested_hash_spec.rb
@@ -1,8 +1,8 @@
 require 'nested_hash'
 
 describe NestedHash do
-  let(:input) { {"loc.x" => 1, "loc.y" => 2, "foo.a" => 10, "foo.b" => 20, "loc.z" => 3}}
-  let(:expected) {{"loc" => {"x" => 1, "y" => 2, "z" => 3}, "foo" => {"a" => 10, "b" => 20}}}
+  let(:input) { {"loc.x" => 1, "loc.y" => 2, "foo.a" => 10, "foo.b" => 20, "foo.c.baz" => 3,}}
+  let(:expected) {{"loc" => {"x" => 1, "y" => 2}, "foo" => {"a" => 10, "b" => 20, "c" => { "baz" => 3}}}}
 
   let(:symbol_keys) { {x:1, y:2}}
   let(:symbol_keys_result) { {'x' => 1, 'y' => 2}}
